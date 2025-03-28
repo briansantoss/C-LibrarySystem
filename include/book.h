6812a34e6f78baef5f6581c1a59be0bd1ca530f0
142011551+briansantoss@users.noreply.github.com <142011551+briansantoss@users.noreply.github.com>
2025-03-27 23:05:50 -0300

    Fix: Corrigindo convenções de código

--- include/book.h
+++ include/book.h
@@ -1,23 +1,23 @@
 #ifndef BOOK_H
 
 typedef struct BookData {
-    char *name;
-    char *author;
+    char* title;
+    char* author;
     double price;
     int quantity;
 } BookData;
 
 typedef struct TreeNode {
     BookData data;
-    struct TreeNode *left;
-    struct TreeNode *right;
+    struct TreeNode* left;
+    struct TreeNode* right;
 } Book;
 
-void insert_book(Book** root_book, char* name, char* author, double price, int quantity);
-void iter_insert_book(Book** root_book, char* name, char* author, double price, int quantity);
-void search_book_name(Book* book, char* target_name);
-void change_book_price(Book* book, char* target_name, double new_price);
-void change_book_quant(Book* book, char* target_name, int new_quantity);
+void insert_book(Book** root_book, char* title, char* author, double price, int quantity);
+void iter_insert_book(Book** root_book, char* title, char* author, double price, int quantity);
+void search_book_name(Book* book, char* target_title);
+void change_book_price(Book* book, char* target_title, double new_price);
+void change_book_quant(Book* book, char* target_title, int new_quantity);
 void inorder_trav(Book* book);
 void free_library(Book* book);
 
--- src/book.c
+++ src/book.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "book.h"
 
-void insert_book(Book **root_book, char* name, char* author, double price, int quantity) {
+void insert_book(Book **root_book, char* title, char* author, double price, int quantity) {
     if (!*root_book) {
         Book *new_book = (Book*) malloc(sizeof (Book));
         if (!new_book) {
@@ -12,24 +12,24 @@ void insert_book(Book **root_book, char* name, char* author, double price, int q
         }
 
         // Reservando espaço para o nome do livro e do autor
-        char* book_name = (char*) malloc(strlen(name) + 1);
+        char* book_title = (char*) malloc(strlen(title) + 1);
         char* book_author = (char*) malloc(strlen(author) + 1);
 
         // Liibera a memória das alocações não bem sucedidas caso alguma falhe
-        if (!book_name || !book_author) {
+        if (!book_title || !book_author) {
             free(new_book);
-            free(book_name);
+            free(book_title);
             free(book_author);
             fprintf(stderr, "\nError: Cannot allocate memory.\n");
             return;
         }
 
         // Preenche a memória alocada com o respectivo conteúdo
-        strcpy(book_name, name);
+        strcpy(book_title, title);
         strcpy(book_author, author);
 
         // Inicializando os campos do livro
-        new_book->data.name = book_name;
+        new_book->data.title = book_title;
         new_book->data.author = book_author;
         new_book->data.price = price;
         new_book->data.quantity = quantity;
@@ -43,21 +43,21 @@ void insert_book(Book **root_book, char* name, char* author, double price, int q
     }
 
     // Armazena o resultado da comperação do nome do alvo com o nome do nó atual
-    int target_cmp = strcmp(name, (*root_book)->data.name);
+    int target_cmp = strcmp(title, (*root_book)->data.title);
     if (target_cmp > 0) {
-        insert_book((&(*root_book)->right), name, author, price, quantity);
+        insert_book((&(*root_book)->right), title, author, price, quantity);
     } else if (target_cmp < 0) {
-        insert_book((&(*root_book)->left), name, author, price, quantity);
+        insert_book((&(*root_book)->left), title, author, price, quantity);
     } else {
         fprintf(stderr, "\nError: Duplicate spotted.\n");
         return;
     }
 }
 
-void iter_insert_book(Book** root_book, char* name, char* author, double price, int quantity) {
+void iter_insert_book(Book** root_book, char* title, char* author, double price, int quantity) {
     Book** curr = root_book;
     while (*curr) {
-        int target_cmp = strcmp(name, (*curr)->data.name);
+        int target_cmp = strcmp(title, (*curr)->data.title);
         if (target_cmp > 0) {
             curr = &(*curr)->right;
         } else if (target_cmp < 0) {
@@ -74,21 +74,21 @@ void iter_insert_book(Book** root_book, char* name, char* author, double price, 
         return;
     }
 
-    char* book_name = (char*) malloc(strlen(name) + 1);
+    char* book_title = (char*) malloc(strlen(title) + 1);
     char* book_author = (char*) malloc(strlen(author) + 1);
 
-    if (!book_name || !book_author) {
+    if (!book_title || !book_author) {
         free(new_book);
-        free(book_name);
+        free(book_title);
         free(book_author);
         fprintf(stderr, "\nError: Cannot allocate memory.\n");
         return;
     }
 
-    strcpy(book_name, name);
+    strcpy(book_title, title);
     strcpy(book_author, author);
 
-    new_book->data.name = book_name;
+    new_book->data.title = book_title;
     new_book->data.author = book_author;
     new_book->data.price = price;
     new_book->data.quantity = quantity;
@@ -98,28 +98,28 @@ void iter_insert_book(Book** root_book, char* name, char* author, double price, 
     *curr = new_book;
 }
 
-void search_book_name(Book *book, char *target_name) {
+void search_book_name(Book* book, char* target_title) {
     if (!book) {
-        fprintf(stderr, "\nError: No book with name %s was found.", target_name);
+        fprintf(stderr, "\nError: No book with name %s was found.", target_title);
         return; // Retorno prévio
     }
     
-    if (strcmp(target_name, book->data.name) == 0) {
-        printf("\nName: %s\nAuthor: %s\nPrice: %.2lf\nQuantity: %d\n", book->data.name, book->data.author, book->data.price, book->data.quantity);
-    } else if (strcmp(target_name, book->data.name) > 0) {
-        search_book_name(book->right, target_name);
+    if (strcmp(target_title, book->data.title) == 0) {
+        printf("\nName: %s\nAuthor: %s\nPrice: %.2lf\nQuantity: %d\n", book->data.title, book->data.author, book->data.price, book->data.quantity);
+    } else if (strcmp(target_title, book->data.title) > 0) {
+        search_book_name(book->right, target_title);
     } else {
-        search_book_name(book->left, target_name);
+        search_book_name(book->left, target_title);
     }
 }
 
-void change_book_price(Book *book, char *target_name, double new_price) {
+void change_book_price(Book* book, char* target_title, double new_price) {
     if (!book) {
-        fprintf(stderr, "\nError: No book with name %s was found.", target_name);
+        fprintf(stderr, "\nError: No book with name %s was found.", target_title);
         return; // Retorno prévio
     }
     
-    if (strcmp(target_name, book->data.name) == 0) {
+    if (strcmp(target_title, book->data.title) == 0) {
         if (book->data.price == new_price) {
             fprintf(stderr, "\nError: The price already is %.2lf.", new_price);
             return; 
@@ -128,36 +128,36 @@ void change_book_price(Book *book, char *target_name, double new_price) {
         book->data.price = new_price;
         printf("\nPrice changed to %.2lf succesfully.", new_price);
         return;
-    } else if (strcmp(target_name, book->data.name) > 0) {
-        change_book_price(book->right, target_name, new_price);
+    } else if (strcmp(target_title, book->data.title) > 0) {
+        change_book_price(book->right, target_title, new_price);
     } else {
-        change_book_price(book->left, target_name, new_price);
+        change_book_price(book->left, target_title, new_price);
     }
 }
 
-void change_book_quant(Book *book, char *target_name, int new_quantity) {
+void change_book_quant(Book* book, char* target_title, int new_quantity) {
     if (!book) {
-        fprintf(stderr, "\nError: No book with name %s was found.", target_name);
+        fprintf(stderr, "\nError: No book with name %s was found.", target_title);
         return; // Retorno prévio
     }
    
-    if (strcmp(target_name, book->data.name) == 0) {
+    if (strcmp(target_title, book->data.title) == 0) {
         if (book->data.quantity == new_quantity) {
             fprintf(stderr, "\nError: The quantity in stock already is %d.", new_quantity);
             return; 
         }
         book->data.quantity = new_quantity;
-    } else if (strcmp(target_name, book->data.name) > 0) {
-        change_book_quant(book->right, target_name, new_quantity);
+    } else if (strcmp(target_title, book->data.title) > 0) {
+        change_book_quant(book->right, target_title, new_quantity);
     } else {
-        change_book_quant(book->left, target_name, new_quantity);
+        change_book_quant(book->left, target_title, new_quantity);
     }
 }
 
-void inorder_trav(Book *book) {
+void inorder_trav(Book* book) {
     if (book) {
         inorder_trav(book->left);
-        printf("\n%s", book->data.name);
+        printf("\n%s", book->data.title);
         inorder_trav(book->right);
     }
 }
@@ -169,7 +169,7 @@ void free_library(Book *book) {
         free_library(book->right);
 
         // Libera o espaço alocado para o nome do livro e o autor
-        free(book->data.name);
+        free(book->data.title);
         free(book->data.author); 
 
         // Libera a raiz
